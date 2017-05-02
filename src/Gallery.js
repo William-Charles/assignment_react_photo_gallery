eea688d55996a18b49db3180b062b809f80181d7
markmarkyesyes <markhahn545@gmail.com>
2017-05-02 11:14:29 -0500

    added image mapping from photos.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,10 @@
 import React from "react";
+import Images from "./Images";
 
 const Gallery = () => {
   return (
     <div>
-      <img
-        src="https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/11934634_844560522259226_320398962_n.jpg"
-        alt="Smiley face"
-      />
+      <Images />
     </div>
   );
 };
--- src/Images.js
+++ src/Images.js
@@ -1,2 +1,19 @@
-//map over data { images }
+import React from "react";
 const photos = require("./photos");
+
+const Images = () => {
+  const images = photosJSON => {
+    photosJSON["data"].map(photo => {
+      return <img src={photo.images.low_resolution.url} />;
+    });
+  };
+  let imageArray = images(photos);
+
+  return (
+    <div>
+      {imageArray}
+    </div>
+  );
+};
+
+export default Images;
