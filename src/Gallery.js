8f4aa7f92737dce9307861e47ce963761d2a3b15
William Whitworth <william10charles@gmail.com>
2017-05-02 19:08:57 -0600

    sort by date feature is working

--- src/Gallery.js
+++ src/Gallery.js
@@ -12,6 +12,7 @@ class Gallery extends React.Component {
       allPhotos: photos.data,
       filteredPhotos: photos.data,
       filter: "all",
+      order: 1,
       page: {
         currentPage: 1,
         maxPage: 2
@@ -23,6 +24,7 @@ class Gallery extends React.Component {
     this._onClickHandler = this._onClickHandler.bind(this);
     this.previousPageHandler = this.previousPageHandler.bind(this);
     this.nextPageHandler = this.nextPageHandler.bind(this);
+    this.sortClickHandler = this.sortClickHandler.bind(this);
   }
 
   _onClickHandler(e) {
@@ -30,6 +32,27 @@ class Gallery extends React.Component {
       filter: e.target.value,
       filteredPhotos: this.filterPhotos(e.target.value)
     });
+  }
+
+  sortClickHandler() {
+    this.setState({
+      order: this.state.order * -1,
+      filteredPhotos: this.sortByUsers()
+    });
+  }
+
+  sortByUsers() {
+    let array;
+    if (this.state.order > 0) {
+      array = this.state.allPhotos.sort(function(a, b) {
+        return a.created_time - b.created_time;
+      });
+    } else {
+      array = this.state.allPhotos.sort(function(a, b) {
+        return b.created_time - a.created_time;
+      });
+    }
+    return array;
   }
 
   filterPhotos(filterParam) {
@@ -54,8 +77,6 @@ class Gallery extends React.Component {
     if (this.state.page.currentPage === this.state.page.maxPage) {
       stop = this.filteredPhotosLength();
     }
-    console.log(start);
-    console.log(stop);
     return this.state.filteredPhotos.slice(start, stop);
   }
 
@@ -89,6 +110,7 @@ class Gallery extends React.Component {
       <div>
         <h2>Gallery</h2>
         <h4>Filter Results: {this.filteredPhotosLength()}</h4>
+        <Button handler={this.sortClickHandler} text={"Sort By Time"} />
         <Select options={this.filters} handler={this._onClickHandler} />
         <Button handler={this.previousPageHandler} text={"Previous"} />
         <Button handler={this.nextPageHandler} text={"Next"} />
